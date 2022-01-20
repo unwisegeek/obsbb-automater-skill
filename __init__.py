629a9a74ae87647d1793b153ac740c4a4183c457
John Madon <john@unwisegeek.net>
2022-01-19 21:25:28 -0500

    Somewhat working code, defined settings, and defined external packages.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 class ObsbbAutomater(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('automater.obsbb.intent')
+    @intent_file_handler('automations.available.intent')
+    def handle_list_automations(self, message):
+        self.speak_dialog('automations.available')
+
+    # @intent_file_handler('automater.obsbb.intent')
+    @intent_handler(IntentBuilder('ExecBBAutomation')
+                    .require('Automation'))
     def handle_automater_obsbb(self, message):
         self.speak_dialog('automater.obsbb')
 
