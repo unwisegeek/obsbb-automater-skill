e9fee64048a79217418241dc74e5a713daae3b04
John Madon <john@unwisegeek.net>
2022-01-19 22:05:35 -0500

    Added interactions to list available automations

--- __init__.py
+++ __init__.py
@@ -1,15 +1,31 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
+import requests
 
 class ObsbbAutomater(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.API_HOST = self.settings.get('api_host', False)
+        self.API_PORT = self.settings.get('api_port', False)
 
     @intent_file_handler('automations.available.intent')
     def handle_list_automations(self, message):
-        self.speak_dialog('automations.available')
+        if self.API_HOST and self.API_PORT:
+            entries = []
+            available_triggers = ""
+            r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation/triggers')
 
-    # @intent_file_handler('automater.obsbb.intent')
+            if r.status_code == 200:    
+                for item in r.text.strip('"').split(','):
+                    entries += [ item.split(":")[0] ]
+                for item in entries:
+                    available_triggers += f"and {item}" if item == entries[-1] else f"{item}, "
+            else:
+                self.speak_dialog("The A P I Call has failed. Please recheck that the A P I is running and the settings are correct.")                
+            self.speak_dialog(f'The available automations are: {available_triggers}')
+        else:
+            self.speak_dialog('Please sign in to yore mycroft dot hey eye account and set the variables for A P I Host and A P I Port in the Skills Setting.')
+
     @intent_handler(IntentBuilder('ExecBBAutomation')
                     .require('Automation'))
     def handle_automater_obsbb(self, message):
