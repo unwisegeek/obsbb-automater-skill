ef17090c4d77389782de87e001af77ffebb90e09
John Madon <john@unwisegeek.net>
2022-01-21 20:22:45 -0500

    Added automation and cheer handlers

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
+from enum import auto
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 import requests
+import json
 
 class ObsbbAutomater(MycroftSkill):
     def __init__(self):
@@ -12,24 +14,67 @@ class ObsbbAutomater(MycroftSkill):
     def handle_list_automations(self, message):
         if self.API_HOST and self.API_PORT:
             entries = []
+            cached = []
             available_triggers = ""
             r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation/triggers')
 
             if r.status_code == 200:    
                 for item in r.text.strip('"').split(','):
                     entries += [ item.split(":")[0] ]
+                    cached += [ dict(
+                        name=item.split(":")[0], 
+                        endpoint=item.split(":")[1]
+                        )]
+                with open('./automation-cache', 'w') as f:
+                    f.write(json.dumps(cached))
                 for item in entries:
                     available_triggers += f"and {item}" if item == entries[-1] else f"{item}, "
+                self.speak_dialog(f'The available automations are: {available_triggers}')
             else:
                 self.speak_dialog("The A P I Call has failed. Please recheck that the A P I is running and the settings are correct.")                
-            self.speak_dialog(f'The available automations are: {available_triggers}')
         else:
             self.speak_dialog('Please sign in to yore mycroft dot hey eye account and set the variables for A P I Host and A P I Port in the Skills Setting.')
 
     @intent_handler(IntentBuilder('ExecBBAutomation')
-                    .require('Automation'))
+                    .require('automations')
+                    .require('activate')
+    )
     def handle_automater_obsbb(self, message):
-        self.speak_dialog('automater.obsbb')
+        endpoint = None
+        pattern = ""
+        translations = {
+            "Start Stream": "stream",
+            "Countdown for 1 Minute": "countdown for one minute",
+            "Countdown for 2 Minutes": "countdown for two minutes",
+            "Countdown for 5 Minutes": "countdown for five minutes. Get me a snack too.",
+            "Countdown for 10 Minutes": "countdown for ten minutes. Someone has to hit the can.",
+            "Go On Camera": "go on camera automation",
+            "Outro": "outro",
+        }
+        target = message.data.get('automations')
+        for key in translations.keys():
+            if key.lower() == target:
+                pattern = translations[key]
+                break
+        self.speak_dialog(f"I'm starting the damned {pattern}")
+        r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation/triggers')
+        if r.status_code == 200:    
+            for item in r.text.strip('"').split(','):
+                if item.split(':')[0].lower() == target:
+                    endpoint = item.split(':')[1]
+                    break
+            if endpoint:
+                r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation?trigger={endpoint}')
+            else:
+                self.speak_dialog('There was an error triggering the automation.')
+        else:
+            self.speak_dialog('There was an error retrieving automations.')
+
+    @intent_file_handler('cheer.intent')
+    def handle_cheer(self, message):
+        self.speak_dialog('Hooray, you.')
+        r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/sound?name=ES_Crowd Teens Cheer 7 -0')
+
 
 
 def create_skill():
