89c6a393fb4e21a50e9313633870cab2b96e1673
John Madon <john@unwisegeek.net>
2022-05-04 19:52:38 -0400

    Catching up on committing code

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 import requests
 import json
+from time import time_ns
+import os
 
 class ObsbbAutomater(MycroftSkill):
     def __init__(self):
@@ -15,20 +17,30 @@ class ObsbbAutomater(MycroftSkill):
         if self.API_HOST and self.API_PORT:
             entries = []
             cached = []
+            phrase = []
             available_triggers = ""
             r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation/triggers')
 
-            if r.status_code == 200:    
+            if r.status_code == 200:
+                cached += [ time_ns() ]
                 for item in r.text.strip('"').split(','):
-                    entries += [ item.split(":")[0] ]
+                    entries += [ item.split(":")[2] ]
                     cached += [ dict(
                         name=item.split(":")[0], 
-                        endpoint=item.split(":")[1]
+                        endpoint=item.split(":")[1],
+                        phrase=item.split(":")[2],
                         )]
                 with open('./automation-cache', 'w') as f:
                     f.write(json.dumps(cached))
-                for item in entries:
-                    available_triggers += f"and {item}" if item == entries[-1] else f"{item}, "
+                f.close()
+                if os.path.exists('./skills/obsbb-automater-skill/locale/en-us/automations.voc'):
+                    self.log.info("removing automations.voc....")
+                    os.remove('./skills/obsbb-automater-skill/locale/en-us/automations.voc')
+                with open('./skills/obsbb-automater-skill/locale/en-us/automations.voc', 'w') as f:
+                    for item in entries:
+                        f.write(f"{item}\n")
+                        available_triggers += f"and {item}" if item == entries[-1] else f"{item}, "
+                f.close()
                 self.speak_dialog(f'The available automations are: {available_triggers}')
             else:
                 self.speak_dialog("The A P I Call has failed. Please recheck that the A P I is running and the settings are correct.")                
@@ -40,42 +52,29 @@ class ObsbbAutomater(MycroftSkill):
                     .require('activate')
     )
     def handle_automater_obsbb(self, message):
+        current_time = time_ns()
         endpoint = None
-        pattern = ""
-        translations = {
-            "Start Stream": "stream",
-            "Countdown for 1 Minute": "countdown for one minute",
-            "Countdown for 2 Minutes": "countdown for two minutes",
-            "Countdown for 5 Minutes": "countdown for five minutes. Get me a snack too.",
-            "Countdown for 10 Minutes": "countdown for ten minutes. Someone has to hit the can.",
-            "Go On Camera": "go on camera automation",
-            "Outro": "outro",
-        }
+        phrase = ""
         target = message.data.get('automations')
-        for key in translations.keys():
-            if key.lower() == target:
-                pattern = translations[key]
+        with open('./automation-cache', 'r') as f:
+            entries = json.loads(f.read())
+            self.log.info(entries)
+        for i in range(1, len(entries)):
+            self.log.info(f"Comparing {entries[i]['phrase'].lower()} to {target.lower()}")
+            if entries[i]["phrase"].lower() == target.lower():
+                endpoint = entries[i]["endpoint"]
+                phrase = entries[i]["phrase"]
                 break
-        self.speak_dialog(f"I'm starting the damned {pattern}")
-        r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation/triggers')
-        if r.status_code == 200:    
-            for item in r.text.strip('"').split(','):
-                if item.split(':')[0].lower() == target:
-                    endpoint = item.split(':')[1]
-                    break
-            if endpoint:
-                r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation?trigger={endpoint}')
-            else:
-                self.speak_dialog('There was an error triggering the automation.')
-        else:I'm
-            self.speak_dialog('There was an error retrieving automations.')
+        if endpoint:
+            self.speak_dialog(f"I'm starting the {phrase}")
+            r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/automation?trigger={endpoint}')
+        else:
+            self.speak_dialog('There was an error triggering the automation.')
 
     @intent_file_handler('cheer.intent')
     def handle_cheer(self, message):
         self.speak_dialog('Hooray, you.')
         r = requests.get(f'http://{self.API_HOST}:{self.API_PORT}/api/sound?name=ES_Crowd Teens Cheer 7 -0')
-
-
 
 def create_skill():
     return ObsbbAutomater()
